882626d50bea8b335c4e57f0e94e096de670a696
saifularafat <saifularafat.info@gmail.com>
2023-07-23 21:19:36 +0600

    the api in testimonials create

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ async function run() {
             res.send(result)
         })
 
-
+        // testimonials api 
+        app.get('/testimonials', async (req, res) => {
+            const result = await testimonialCollection.find().toArray();
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
